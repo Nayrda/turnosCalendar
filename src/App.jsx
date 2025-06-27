2e6b09f1ffd6528ca373c2610ec415915b55284b
Nayrda <nayrda13@gmail.com>
2025-06-27 15:27:43 +0200

    last update

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CalendarShiftsApp from './turnos.jsx'; // Asegúrate de que la ruta sea correcta
+
+function App() {
+  return (
+    <div className="App">
+      <CalendarShiftsApp />
+    </div>
+  );
+}
+
+export default App;
